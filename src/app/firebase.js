dab919af96d753d7c8796f00bfd6e1235983c68c
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-16 19:04:04 +0100

    install firebase

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpWMltKh0zhq7gyNwyvUMLvWDssSAQOug",
+  authDomain: "api-the-gospel-voice.firebaseapp.com",
+  projectId: "api-the-gospel-voice",
+  storageBucket: "api-the-gospel-voice.appspot.com",
+  messagingSenderId: "648624041809",
+  appId: "1:648624041809:web:a8e7f8a1672c235b83ed70"
+};
+
+// Initialize Firebase
+export default app = initializeApp(firebaseConfig);
