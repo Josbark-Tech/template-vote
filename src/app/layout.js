57c9422d4ee1186a38df1c9f7ded4d8d108d53fe
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-16 19:53:17 +0100

    netoyage fichier

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+//import './globals.css'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'The Gospel voice',
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   )
 }
