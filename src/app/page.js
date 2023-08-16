9000852e7ab9df5ebf6434b12292bcf4335a4105
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-16 18:07:39 +0100

    ajout zustand et test tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,16 @@
-import Image from 'next/image'
+//import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <div className="text-red-500 bg-blue-300"> Hello tailwind</div>
+      
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      
       </div>
 
       <div className={styles.grid}>
@@ -70,9 +44,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
+         
           <p>Explore the Next.js 13 playground.</p>
         </a>
 
