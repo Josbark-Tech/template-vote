759dbb0f6bacab132a0186e6593f676deacf8ae1
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-17 13:45:06 +0100

    mettre le lien

--- src/app/admin/components/layouts/Sidebar.js
+++ src/app/admin/components/layouts/Sidebar.js
@@ -6,18 +6,14 @@ import { FaUsers } from "react-icons/fa";
 import { FaUserGraduate } from "react-icons/fa";
 import { AiTwotoneSetting } from "react-icons/ai";
 import { RiLogoutBoxRFill } from "react-icons/ri";
-
-//
-// import Profile from "./Profile";
-// import { Link } from "react-router-dom";
-// import Search from "./Search";
+import Link from "next/link";
 
 const listMenu = [
-  { nom: "Acceuil", icon: BiSolidHomeAlt2 },
-  { nom: "Candidat", icon: "" },
-  { nom: "Electeur", icon: "" },
-  { nom: "Parametre", icon: "" },
-  { nom: "Quitter", icon: "" },
+  { nom: "Acceuil", icon: BiSolidHomeAlt2 ,link:"/admin"},
+  { nom: "Candidat", icon: FaUserGraduate ,link:"/admin/candidat"},
+  { nom: "Electeur", icon: FaUsers ,link:"/admin/elector"},
+  { nom: "Parametre", icon: AiTwotoneSetting,link:"/admin" },
+  { nom: "Quitter", icon: RiLogoutBoxRFill ,link:"/admin"},
 ];
 export default function Siderbar() {
   // const [isOpen, setIsOpen] = useState(false);
@@ -26,29 +22,13 @@ export default function Siderbar() {
 
   return (
     <aside class="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
-      
-      <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-black hover:duration-300 hover:ease-linear focus:bg-white">
-        <BiSolidHomeAlt2 size={36} className="text-white" />
-      </div>
-      {/* <!-- Courses --> */}
-      <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-black  hover:duration-300 hover:ease-linear focus:bg-white">
-        <FaUserGraduate size={36} className="text-white" />
-        {/* <label for="">Candidat</label> */}
-      </div>
-
-      {/* <!-- Theme --> */}
-      <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-        <FaUsers size={36} className="text-white" />{" "}
-        {/* <label for="">Electeur</label> */}
-      </div>
-
-      {/* <!-- Configuration --> */}
-      <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-        <AiTwotoneSetting size={36} className="text-white" />
-      </div>
-      <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-        <RiLogoutBoxRFill size={36} className="text-white" />
-      </div>
+      {listMenu.map((item, key) => (
+        <Link  href={item.link} key={key}>
+          <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-black hover:duration-300 hover:ease-linear focus:bg-white">
+            <item.icon size={36} className="text-white" />
+          </div>
+        </Link>
+      ))}
     </aside>
   );
 }
