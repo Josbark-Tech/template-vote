5bb1b916b132a1d59bcf10bdaf1c9e21f83993db
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-17 14:27:13 +0100

    refactor tableau

--- src/app/admin/components/Tableau.js
+++ src/app/admin/components/Tableau.js
@@ -1,5 +1,7 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from "react";
+
+import { AiOutlineSearch } from "react-icons/ai";
+import Thead from "./Thead";
 
 export default function Tableau() {
   return (
@@ -17,41 +19,22 @@ export default function Tableau() {
                   <option>10</option>
                   <option>20</option>
                 </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </div>
               </div>
               <div className="relative">
                 <select className="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                  <option>All</option>
-                  <option>Active</option>
-                  <option>Inactive</option>
+                  <option>tous</option>
+                  <option>Bien</option>
+                  <option>Moyenne</option>
+                  <option>Eliminé</option>
                 </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </div>
               </div>
             </div>
             <div className="block relative">
               <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                <svg
-                  viewBox="0 0 24 24"
-                  className="h-4 w-4 fill-current text-gray-500"
-                >
-                  <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
-                </svg>
+                <AiOutlineSearch
+                  size={24}
+                  className="fill-current text-gray-500"
+                />
               </span>
               <input
                 placeholder="Search"
@@ -62,7 +45,8 @@ export default function Tableau() {
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
               <table className="min-w-full leading-normal">
-                <thead>
+                <Thead />
+                {/* <thead>
                   <tr>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                       Candidat
@@ -77,7 +61,7 @@ export default function Tableau() {
                       Tendances
                     </th>
                   </tr>
-                </thead>
+                </thead> */}
                 <tbody>
                   <tr>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -100,9 +84,7 @@ export default function Tableau() {
                       <p className="text-gray-900 whitespace-no-wrap">100</p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        2020
-                      </p>
+                      <p className="text-gray-900 whitespace-no-wrap">2020</p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                       <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
@@ -135,9 +117,7 @@ export default function Tableau() {
                       <p className="text-gray-900 whitespace-no-wrap">100</p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                      100
-                      </p>
+                      <p className="text-gray-900 whitespace-no-wrap">100</p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                       <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
@@ -170,9 +150,7 @@ export default function Tableau() {
                       <p className="text-gray-900 whitespace-no-wrap">100</p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                      100
-                      </p>
+                      <p className="text-gray-900 whitespace-no-wrap">100</p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                       <span className="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
@@ -205,9 +183,7 @@ export default function Tableau() {
                       <p className="text-gray-900 whitespace-no-wrap">100</p>
                     </td>
                     <td className="px-5 py-5 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        18
-                      </p>
+                      <p className="text-gray-900 whitespace-no-wrap">18</p>
                     </td>
                     <td className="px-5 py-5 bg-white text-sm">
                       <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
