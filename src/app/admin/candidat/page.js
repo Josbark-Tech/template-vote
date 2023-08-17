9b1b8c56f17145a247edfdd7f816619565d5d518
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-17 11:46:27 +0100

    formulaire ajouter candidat

--- src/app/admin/candidat/page.js
+++ src/app/admin/candidat/page.js
@@ -4,17 +4,18 @@ import React from "react";
 
 import Main from "../components/layouts/Page";
 import Tableau from "../components/Tableau";
+import Form from "../components/candidat/Form";
 
-export default function Candidate() {
-
-
+export default function Candidat() {
   return (
+
     <Main>
       <div class="h-full w-full m-4 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-max gap-4 overflow-y-scroll">
         <div class="container items-center px-4 py-8 m-auto mt-5">
-          <div class="flex flex-wrap pb-3 mx-4 md:mx-24 lg:mx-0">
-            <Tableau />
-            <Tableau />
+          <div class="flex flex-wrap ">
+            <div className="mx-4"> <Tableau /></div>
+           
+            <Form />
           </div>
         </div>
       </div>
---
+++ src/app/admin/components/candidat/Form.js
@@ -0,0 +1,155 @@
+import React from "react";
+// import logo from "../../images/yakka.png";
+export default function Form() {
+  return (
+    <section class=" w-full max-w-lg  bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="bg-white dark:bg-gray-900">
+        <div className="max-w-2xl px-4 mx-auto lg:py-16">
+          <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+            Ajouter un candidat
+          </h2>
+          <form action="#">
+            {/* <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5"> */}
+            <div className=" mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
+              <div className="lg:col-span-2 flex">
+                <div className="mx-2">
+                  <img
+                    className="w-10 h-10  rounded"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                    alt="Medium large avatar"
+                  />
+                </div>
+                <div>
+                  <input
+                    class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    id="file_input"
+                    type="file"
+                  />
+                </div>
+              </div>
+              <div>
+                <div class="flex py-3 items-center justify-center w-full">
+                  <label
+                    for="dropzone-file"
+                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  >
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg
+                        class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                      </svg>
+                      <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span class="font-semibold">Click to upload</span> or
+                        drag and drop
+                      </p>
+                      <p class="text-xs text-gray-500 dark:text-gray-400">
+                        SVG, PNG, JPG or GIF (MAX. 800x400px)
+                      </p>
+                    </div>
+                    <input id="dropzone-file" type="file" class="hidden" />
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center my-2 space-x-4">
+              <div className="w-full">
+                <label
+                  for="price"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Votre nom
+                </label>
+
+                <input
+                  className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
+                  type="text"
+                  value=""
+                  onChange={(e) => console.log(e.target.value)}
+                  // {...getFieldProps("email")}
+                  placeholder="Votre adresse email ou nom d'utilisateur"
+                  required
+                />
+              </div>
+            </div>
+            <div className="flex items-center my-2 space-x-4">
+              <div className="w-full">
+                <label
+                  for="price"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Votre nom
+                </label>
+
+                <input
+                  className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
+                  type="text"
+                  value=""
+                  onChange={(e) => console.log(e.target.value)}
+                  // {...getFieldProps("email")}
+                  placeholder="Votre adresse email ou nom d'utilisateur"
+                  required
+                />
+              </div>
+            </div>
+            <div className="flex items-center my-2  space-x-4">
+              <button
+                type="button"
+                className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+              >
+                <svg
+                  className="w-5 h-5 mr-1 -ml-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Delete
+              </button>
+              <button
+                type="button"
+                className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+              >
+                <svg
+                  className="w-5 h-5 mr-1 -ml-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Delete
+              </button>
+
+              <button
+                type="button"
+                class="px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Large
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
