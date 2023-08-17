ee854a6676af3713d7405064f21c3246919f9448
Bayekula-Hb <hobedbayekula@gmail.com>
2023-08-17 10:19:15 +0100

    feat: add section

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,18 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className=''>
+      <section className="container--intro flex items-center p-4">
+        <div className='voice-presentation'>
+          <h2 className='uppercase mb-2'>
+              The Gospel Voice
+          </h2>
+          <p>
+            A la recherche des meilleurs tallents Gospel dans les differentes villes de la Republique Democratique du Congo
+          </p>
+
+        </div>
+      </section>
     </main>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,13 @@
 
-export default function HomePage(){
+export default function Home(){
 
   return (
     <>
       <main className="container">
-
+        <h1>Title Title</h1>
+        <h1>Title Title</h1>
+        <h1>Title Title</h1>
+        <h1>Title Title</h1>
       </main>
       
     </>
