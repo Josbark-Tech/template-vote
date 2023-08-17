ce1dcb59ae932ef71b0364922e8293b6a77196cd
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-17 20:14:52 +0100

    ajout image

--- src/app/admin/components/candidat/Form.js
+++ src/app/admin/components/candidat/Form.js
@@ -1,10 +1,84 @@
-import React from "react";
+'use client'
 
-
+import React, { useState } from "react";
+// import { GetServerSideProps, NextPage } from "next";
+// import path from "path";
+// import fs from "fs/promises";
+// import axios from "axios";
+import {
+  collection,
+  addDoc,
+  // query,
+  // onSnapshot,
+  // deleteDoc,
+  // doc,
+  /*getDoc,
+  querySnapshot,
+ */
+} from "firebase/firestore";
+// import data from "../../../api/upload"
+import { db } from "../../../firebase";
 
 export default function Form() {
+  const [uploading, setUploading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
 
+  const [candidates, SetCandidate] = useState([]);
+  const [newItem, setNewItem] = useState({
+    nom: "",
+    prenom: "",
+    image: "",
+    like: 0,
+    islike: 0,
+  });
 
+  const handleUpload = async () => {
+    setUploading(true);
+    try {
+      if (!selectedFile) return;
+      const data = new FormData();
+      data.set('file', selectedFile)
+      
+      const res = await fetch('../../../api/upload',{
+        method:'POST',
+        body: data
+      });
+      console.log(res);
+
+      if(!res.ok){
+        throw new Error(await res.text());
+      }else{
+        setUploading(false);
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(uploading)
+    
+  };
+  // Add candidat to database
+  const addItem = async (e) => {
+    try {
+      e.preventDefault();
+
+      SetCandidate([...candidates, newItem]);
+
+      if (newItem.nom !== "" && newItem.prenom !== "") {
+        console.log(newItem);
+        SetCandidate([...candidates, newItem]);
+        await addDoc(collection(db, "candidates"), {
+          nom: newItem.nom.trim(),
+          prenom: newItem.prenom.trim(),
+          image: "",
+          like: 0,
+          islike: 0,
+        });
+        setNewItem({ nom: "", prenom: "", like: 0, islike: 0 });
+      }
+    } catch (e) {}
+  };
   return (
     <section className=" w-full max-w-lg  bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
       <div className="bg-white dark:bg-gray-900">
@@ -17,14 +91,30 @@ export default function Form() {
             <div className=" mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
               <div className="lg:col-span-2 flex">
                 <div className="mx-2">
-                  <img
-                    className="w-10 h-10  rounded"
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                    alt="Medium large avatar"
-                  />
+                  {selectedImage ? (
+                    <img
+                      className="w-10 h-10  rounded"
+                      src={selectedImage}
+                      alt="Medium large avatar"
+                    />
+                  ) : (
+                    <img
+                      className="w-10 h-10  rounded"
+                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                      alt="Medium large avatar"
+                    />
+                  )}
                 </div>
                 <div>
                   <input
+                    onClick={handleUpload}
+                    onChange={({ target }) => {
+                      if (target.files) {
+                        const file = target.files[0];
+                        setSelectedImage(URL.createObjectURL(file));
+                        setSelectedFile(file);
+                      }
+                    }}
                     className="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                     id="file_input"
                     type="file"
@@ -55,8 +145,8 @@ export default function Form() {
                       </svg>
                       {/* <IoPersonAddSharp size={36} className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" /> */}
                       <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span className="font-semibold">Click to upload</span> or
-                        drag and drop
+                        <span className="font-semibold">Click to upload</span>{" "}
+                        or drag and drop
                       </p>
                       <p className="text-xs text-gray-500 dark:text-gray-400">
                         SVG, PNG, JPG or GIF (MAX. 800x400px)
@@ -73,16 +163,17 @@ export default function Form() {
                   for="price"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Votre nom
+                  Votre prénom
                 </label>
 
                 <input
                   className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
                   type="text"
-                  value=""
-                  onChange={(e) => console.log(e.target.value)}
-                  // {...getFieldProps("email")}
-                  placeholder="Votre adresse email ou nom d'utilisateur"
+                  value={newItem.prenom}
+                  onChange={(e) =>
+                    setNewItem({ ...newItem, prenom: e.target.value })
+                  }
+                  placeholder="prénom"
                   required
                 />
               </div>
@@ -99,10 +190,11 @@ export default function Form() {
                 <input
                   className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
                   type="text"
-                  value=""
-                  onChange={(e) => console.log(e.target.value)}
-                  // {...getFieldProps("email")}
-                  placeholder="Votre adresse email ou nom d'utilisateur"
+                  value={newItem.nom}
+                  onChange={(e) =>
+                    setNewItem({ ...newItem, nom: e.target.value })
+                  }
+                  placeholder="Nom"
                   required
                 />
               </div>
@@ -115,6 +207,7 @@ export default function Form() {
                 Modifier
               </button>
               <button
+                onClick={addItem}
                 type="button"
                 className="px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
@@ -126,4 +219,14 @@ export default function Form() {
       </div>
     </section>
   );
-}
+};
+// export const getServerSideProps = async () => {
+//   const props = { dirs: [] };
+//   try {
+//     const dirs = await fs.readdir(path.join(process.cwd(), "/public/images"));
+//     props.dirs = dirs ;
+//     return { props };
+//   } catch (error) {
+//     return { props };
+//   }
+// };
