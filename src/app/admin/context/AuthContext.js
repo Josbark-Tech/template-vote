206811031526d12143629f6a28d629cc24c5bc2d
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-17 05:06:41 +0100

    refactor context

---
+++ src/app/admin/context/AuthContext.js
@@ -0,0 +1,49 @@
+"use client";
+
+//import axios from "axios";
+import { useRouter } from "next/navigation";
+import { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  const registerUser = async ({ name, email, password }) => {
+    try {
+      const data = {};
+      data.user = name;
+      data.email = email;
+      data.password = password;
+  
+      if (data?.user) {
+        router.push("/");
+      }
+    } catch (error) {
+      setError(error?.response?.data?.message);
+    }
+  };
+
+  const clearErrors = () => {
+    setError(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        error,
+        setUser,
+        registerUser,
+        clearErrors,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,7 +1,21 @@
-import '../globals.css'
-import './page.module.css'
 import React from "react";
+import Header from "./components/layouts/Header";
+import { GlobalProvider } from "./GlobalProvider";
+import '../globals.css';
+import './page.module.css';
 
-export default function Templates({ children }) {
-  return <div className>{children}</div>;
+
+export default function RootLayout({ children }) {
+  //return <div className>{children}</div>;
+  return (
+    <html lang="fr">
+      <head />
+      <body>
+        <GlobalProvider>
+          <Header />
+          {children}
+        </GlobalProvider>
+      </body>
+    </html>
+  );
 }
