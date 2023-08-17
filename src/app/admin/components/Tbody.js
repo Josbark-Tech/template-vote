5234719ba7ec8ef2157bfac2f31750f45a7f5bdd
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-17 14:41:43 +0100

    fixe head

---
+++ src/app/admin/components/Tbody.js
@@ -0,0 +1,132 @@
+import React from "react";
+
+export default function Tbody() {
+  return (
+    <tbody>
+      <tr>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 w-10 h-10">
+              <img
+                className="w-full h-full rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <p className="text-gray-900 whitespace-no-wrap">Vera Carpenter</p>
+            </div>
+          </div>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">100</p>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">2020</p>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+            ></span>
+            <span className="relative">Bien</span>
+          </span>
+        </td>
+      </tr>
+      <tr>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 w-10 h-10">
+              <img
+                className="w-full h-full rounded-full"
+                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <p className="text-gray-900 whitespace-no-wrap">Blake Bowman</p>
+            </div>
+          </div>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">100</p>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">100</p>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+            ></span>
+            <span className="relative">Bien</span>
+          </span>
+        </td>
+      </tr>
+      <tr>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 w-10 h-10">
+              <img
+                className="w-full h-full rounded-full"
+                src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <p className="text-gray-900 whitespace-no-wrap">Dana Moore</p>
+            </div>
+          </div>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">100</p>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">100</p>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <span className="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
+            ></span>
+            <span className="relative">Moyenne</span>
+          </span>
+        </td>
+      </tr>
+      <tr>
+        <td className="px-5 py-5 bg-white text-sm">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 w-10 h-10">
+              <img
+                className="w-full h-full rounded-full"
+                src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <p className="text-gray-900 whitespace-no-wrap">Alonzo Cox</p>
+            </div>
+          </div>
+        </td>
+        <td className="px-5 py-5 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">100</p>
+        </td>
+        <td className="px-5 py-5 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">18</p>
+        </td>
+        <td className="px-5 py-5 bg-white text-sm">
+          <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
+            ></span>
+            <span className="relative"> Eliminé</span>
+          </span>
+        </td>
+      </tr>
+    </tbody>
+  );
+}
--- src/app/admin/components/layouts/Header.js
+++ src/app/admin/components/layouts/Header.js
@@ -6,18 +6,18 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="h-16 w-full flex items-center relative justify-end px-5 space-x-10 bg-gray-800">
+    <header class="h-16 w-full flex items-center relative justify-end px-5 space-x-10 bg-gray-800">
       {/* <!-- Informação --> */}
-      <div className="flex flex-shrink-0 items-center space-x-4 text-white">
+      <div class="flex flex-shrink-0 items-center space-x-4 text-white">
         {/* <!-- Texto --> */}
-        <div className="flex flex-col items-end ">
+        <div class="flex flex-col items-end ">
           {/* <!-- Nome --> */}
-          <div className="text-md font-medium ">Jacokash</div>
+          <div class="text-md font-medium ">Jacokash</div>
           {/* <!-- Título --> */}
-          <div className="text-sm font-regular">Admni</div>
+          <div class="text-sm font-regular">Admni</div>
         </div>
         {/* <!-- Foto --> */}
-        <div className="h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400"></div>
+        <div class="h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400"></div>
       </div>
     </header>
   );
