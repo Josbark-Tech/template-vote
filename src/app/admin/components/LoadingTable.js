2c5bc1307d1947ffe420613223102657c080e6d1
JacobitKashala <Jacobitkashala@gmail.com>
2023-08-19 13:49:57 +0100

    fix image

--- src/app/admin/components/LoadingTable.js
+++ src/app/admin/components/LoadingTable.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoandingTableau({nbre}) {
   return (
-    <tbody class="text-center">
+    <tbody className="text-center">
       <tr role="status">
         <td
           colspan={nbre}
@@ -10,7 +10,7 @@ export default function LoandingTableau({nbre}) {
         >
           <svg
             aria-hidden="true"
-            class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +24,7 @@ export default function LoandingTableau({nbre}) {
               fill="currentFill"
             />
           </svg>
-          <span class="sr-only">Loading...</span>
+          <span className="sr-only">Loading...</span>
         </td>
       </tr>
     </tbody>
--- src/app/admin/components/layouts/Header.js
+++ src/app/admin/components/layouts/Header.js
@@ -6,18 +6,18 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header class="h-16 w-full flex items-center relative justify-end px-5 space-x-10 bg-gray-800">
+    <header className="h-16 w-full flex items-center relative justify-end px-5 space-x-10 bg-gray-800">
       {/* <!-- Informação --> */}
-      <div class="flex flex-shrink-0 items-center space-x-4 text-white">
+      <div className="flex flex-shrink-0 items-center space-x-4 text-white">
         {/* <!-- Texto --> */}
-        <div class="flex flex-col items-end ">
+        <div className="flex flex-col items-end ">
           {/* <!-- Nome --> */}
-          <div class="text-md font-medium ">Jacokash</div>
+          <div className="text-md font-medium ">Jacokash</div>
           {/* <!-- Título --> */}
-          <div class="text-sm font-regular">Admni</div>
+          <div className="text-sm font-regular">Admni</div>
         </div>
         {/* <!-- Foto --> */}
-        <div class="h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400"></div>
+        <div className="h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400"></div>
       </div>
     </header>
   );
